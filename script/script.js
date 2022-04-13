fb723408ad3b304aaba51ad0c3e6385c1510c7cf
Andrey Gomes <andreygdsn@gmail.com>
2022-04-12 23:04:12 -0300

    all to alert

--- script/script.js
+++ script/script.js
@@ -30,27 +30,27 @@ function playRound(pc, player){
     const userp = 0;
     const pcp = 1;
     if(pc==player){
-        console.log(`Draw! ${items[pc]} is equal to ${items[player]}`);
+        alert(`Draw! ${items[pc]} is equal to ${items[player]}`);
     }else if(pc===0 && player===1){
-        console.log(`Player Wins! ${items[player]} beats ${items[pc]}`);
+        alert(`Player Wins! ${items[player]} beats ${items[pc]}`);
         return userp;
     }else if(pc===0 && player===2){
-        console.log(`Computer Wins! ${items[pc]} beats ${items[player]}`);
+        alert(`Computer Wins! ${items[pc]} beats ${items[player]}`);
         return pcp;
     }else if(pc===1 && player===0){
-        console.log(`Computer Wins! ${items[pc]} beats ${items[player]}`);
+        alert(`Computer Wins! ${items[pc]} beats ${items[player]}`);
         return pcp;
     }else if(pc===1 && player===2){
-        console.log(`Player Wins! ${items[player]} beats ${items[pc]}`);
+        alert(`Player Wins! ${items[player]} beats ${items[pc]}`);
         return userp;
     }else if(pc===2 && player===0){
-        console.log(`Player Wins! ${items[player]} beats ${items[pc]}`);
+        alert(`Player Wins! ${items[player]} beats ${items[pc]}`);
         return userp;
     }else if(pc===2 && player===1){
-        console.log(`Computer Wins! ${items[pc]} beats ${items[player]}`);
+        alert(`Computer Wins! ${items[pc]} beats ${items[player]}`);
         return pcp;
     }else{
-        console.log('Something went wrong!');
+        alert('Something went wrong!');
     }
 }
 
@@ -65,13 +65,16 @@ function game(){
         }else if(round === 1){
             computerPoints+=1;
         }
-        console.log(`Player ${playerPoints} X ${computerPoints} Computer`)
+        alert(`Player ${playerPoints} X ${computerPoints} Computer`)
     }
     if(playerPoints > computerPoints){
+        alert(`Player wins the game by ${playerPoints} X ${computerPoints}`);
         console.log(`Player wins the game by ${playerPoints} X ${computerPoints}`);
     }else if(computerPoints > playerPoints){
+        alert(`Computer wins the game by ${computerPoints} X ${playerPoints}`);
         console.log(`Computer wins the game by ${computerPoints} X ${playerPoints}`);
     }else{
+        alert(`It's a Draw! ${playerPoints} X ${computerPoints}`)
         console.log(`It's a Draw! ${playerPoints} X ${computerPoints}`)
     }
 }
