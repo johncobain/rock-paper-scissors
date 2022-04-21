ac217003ad30edda4eabe56d14d2c498c27939df
john cobain <andreygdsn@gmail.com>
2022-04-21 09:36:32 -0300

    ui in javascript

--- script/script.js
+++ script/script.js
@@ -1,57 +1,68 @@
 const items = ['rock', 'paper', 'scissors'];
 
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
 
 function playRound(player){
     let pc = Math.floor(Math.random() * 3);
-    const res = document.querySelector('.result');
-
+    
     console.log(player, pc)
-    const userp = 0;
-    const pcp = 1;
     if(pc==player){
         res.textContent = `Draw! ${items[pc]} is equal to ${items[player]}`;
     }else if(pc===0 && player===1){
         res.textContent = `Player Wins! ${items[player]} beats ${items[pc]}`;
-        return userp;
+        return playerPoints += 1;
     }else if(pc===0 && player===2){
         res.textContent = `Computer Wins! ${items[pc]} beats ${items[player]}`;
-        return pcp;
+        return computerPoints += 1;
     }else if(pc===1 && player===0){
         res.textContent = `Computer Wins! ${items[pc]} beats ${items[player]}`;
-        return pcp;
+        return computerPoints += 1;
     }else if(pc===1 && player===2){
         res.textContent = `Player Wins! ${items[player]} beats ${items[pc]}`;
-        return userp;
+        return playerPoints += 1;
     }else if(pc===2 && player===0){
         res.textContent = `Player Wins! ${items[player]} beats ${items[pc]}`;
-        return userp;
+        return playerPoints += 1;
     }else if(pc===2 && player===1){
         res.textContent = `Computer Wins! ${items[pc]} beats ${items[player]}`;
-        return pcp;
+        return computerPoints += 1;
     }else{
         res.textContent = 'Something went wrong!';
-}
-}
-
-function game(){
-    let playerPoints = 0;
-    let computerPoints = 0;
-
-    if(playerPoints > computerPoints){
-        alert(`Player wins the game by ${playerPoints} X ${computerPoints}`);
-        console.log(`Player wins the game by ${playerPoints} X ${computerPoints}`);
-    }else if(computerPoints > playerPoints){
-        alert(`Computer wins the game by ${computerPoints} X ${playerPoints}`);
-        console.log(`Computer wins the game by ${computerPoints} X ${playerPoints}`);
-    }else{
-        alert(`It's a Draw! ${playerPoints} X ${computerPoints}`)
-        console.log(`It's a Draw! ${playerPoints} X ${computerPoints}`)
     }
 }
 
+function game(player, computer){
+    if(player > computer){
+        para.textContent = `Player wins the game by ${player} X ${computer}`;
+        console.log(`Player wins the game by ${player} X ${computer}`);
+    }else if(computer > player){
+        para.textContent = `Computer wins the game by ${computer} X ${player}`;
+        console.log(`Computer wins the game by ${computer} X ${player}`);
+    }else{
+        para.textContent = `It's a Draw! ${player} X ${computer}`;
+        console.log(`It's a Draw! ${player} X ${computer}`);
+    }    
+}
+
+const res = document.querySelector('.result');
+res.textContent = 'Make a choice';
+const para = document.createElement('p');
+insertAfter(para, res);
+
+let playerPoints = 0;
+let computerPoints = 0;
 const choices = document.querySelectorAll('button');
 choices.forEach((e) =>{
-    e.addEventListener('click', () => playRound(parseInt(e.id)));
-})
+    e.addEventListener('click', () => {
+        playRound(parseInt(e.id));
+        res.innerHTML += `<br>Player ${playerPoints} X ${computerPoints} Computer`
+        para.textContent = '';
+        if(playerPoints === 5|| computerPoints ===5){
+            game(playerPoints, computerPoints);
+            playerPoints = 0;
+            computerPoints = 0;
+        }
+    })});
 
-//game();
